b0c737dd88fca7b2121593a1e00d917f8ec49653
THARUN <93336635+THARUN13055@users.noreply.github.com>
2022-02-26 15:39:44 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,1 @@
-a=input("enter the number of the string:")
-b=input("enter the value of the string:")
-print(a ,":the given value is number of string ")
-print(b,":the enter value of the sting is this")
+print("hlo world")
